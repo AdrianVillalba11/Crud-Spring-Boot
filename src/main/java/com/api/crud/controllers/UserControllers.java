b1f275935226b2543d56deba686d8a0850aee162
Adrian <adrianvillalba70@gmail.com>
2023-04-20 14:29:22 -0500

    Se Edita el UserController con el restcontroller y mapping

--- src/main/java/com/api/crud/controllers/UserControllers.java
+++ src/main/java/com/api/crud/controllers/UserControllers.java
@@ -1,4 +1,16 @@
 package com.api.crud.controllers;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+
+/*
+* ACA DEFINIMOS LAS PETICIONES Y RUTAS
+* */
 public class UserControllers {
+
+
 }
