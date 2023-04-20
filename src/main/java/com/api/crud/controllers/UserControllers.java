d5c85bb153c8dffacbefb87df37b831f5fedda43
Adrian <adrianvillalba70@gmail.com>
2023-04-20 16:49:40 -0500

    Se agrega el get mapping y sus repectivos cambios en Interface

--- src/main/java/com/api/crud/controllers/UserControllers.java
+++ src/main/java/com/api/crud/controllers/UserControllers.java
@@ -1,8 +1,15 @@
 package com.api.crud.controllers;
 
 
+import com.api.crud.models.UserModel;
+import com.api.crud.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/user")
@@ -11,6 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 * ACA DEFINIMOS LAS PETICIONES Y RUTAS
 * */
 public class UserControllers {
+    @Autowired
+    private UserServices userService;
+
+    @GetMapping
+    public ArrayList<UserModel> getUsers(){
+        return this.userService.getUsers();
+    }
+
 
 
 }
--- src/main/java/com/api/crud/services/UserServices.java
+++ src/main/java/com/api/crud/services/UserServices.java
@@ -1,4 +1,24 @@
 package com.api.crud.services;
 
+import com.api.crud.models.UserModel;
+import com.api.crud.repositories.IUserRepository;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class UserServices {
+    @Autowired // Sirve para inyeccion de depedencias
+    IUserRepository userRepository;
+
+    public ArrayList<UserModel>getUsers() {
+        return (ArrayList<UserModel>) userRepository.findAll();
+    }
+
+    public UserModel saveUser(UserModel user){
+        return userRepository.save(user);
+    }
+
 }
